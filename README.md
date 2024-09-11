e75a18313235afd4dc51842519b1b4ca419f0e8a
Gbhanuteja <gbhanuteja_csb223222@mgit.ac.in>
2024-09-11 17:51:54 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Voice-Based-Search-Engine
+# Voice-Based-Search-Engine
---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('start-btn').addEventListener('click',()=> {
+    if ('webkitSpeechRecognition' in window||'SpeechRecognition' in window) {
+        const SpeechRecognition=window.SpeechRecognition||window.webkitSpeechRecognition;
+        const recognition=new SpeechRecognition();
+        recognition.lang='en-US';
+        recognition.start();
+        recognition.onstart=()=>{
+            console.log('Voice recognition started. Speak into the microphone.');
+        };
+        recognition.onspeechend=()=>{
+            recognition.stop();
+        };
+        recognition.onresult=(event)=>{
+            const speechResult=event.results[0][0].transcript.toLowerCase();
+            speak(`showing results for ${speechResult}`);
+            if (speechResult.includes('open calculator')) {
+                speak('Opening calculator');
+                window.open('calculator://');
+            } else {
+                speak(`Searching for ${speechResult}`);
+                const googleSearchURL=`https://www.google.com/search?q=${encodeURIComponent(speechResult)}`;
+                window.location.href=googleSearchURL;
+            }
+        };
+        recognition.onerror=(event)=>{
+            console.error('Error occurred in recognition: ', event.error);
+        };
+    }else{
+        alert('Your browser does not support voice recognition.');
+    }
+});
+function speak(text){
+    const utterance=new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
+}
